e05305a52b236004981a7810603015ef66f61ad8
Sachin Ghait <ssghait.007@gmail.com>
2021-10-16 22:35:34 +0530

    Update update.js

--- netlify/functions/fauna-crud/update.js
+++ netlify/functions/fauna-crud/update.js
@@ -5,7 +5,9 @@ const { query, Client } = require('faunadb')
 
 const client = new Client({
   secret: process.env.FAUNADB_SERVER_SECRET,
-})
+  domain: 'db.us.fauna.com',
+  scheme: 'https',
+});
 
 const handler = async (event) => {
   const data = JSON.parse(event.body)
