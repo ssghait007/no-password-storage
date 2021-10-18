695968c719d47cbbc22cebdd0f2edb34fe8fdf45
Sachin Ghait <ssghait.007@gmail.com>
2021-10-18 22:42:04 +0530

    Update _slug.vue

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>{{ this.slug }}</h1>
-    <div v-if="!this.available">
+    <Header :isActive="this.showRegModal"></Header>
+    <div v-if="this.pageAvailable === 'unavailable'">
       Site is taken. If you are the owner, Enter your password to continue {WHEN
       MODAL CLOSED DECRYPT DATA}
     </div>
-    <div v-else>
+    <div v-if="this.pageAvailable === 'available'">
       Page is available,Enter password to create your site {THIS WILL BE IN
       MODAL}
       <button class="btn btn-blue" @click.prevent="visibleModal()">
@@ -13,63 +13,102 @@
       </button>
 
       <!-- Modal Background -->
-      <div v-if="showModal" class="moda">
+      <div v-if="showRegModal" class="moda">
         <!-- Modal -->
         <div
-          v-if="showModal"
-          class="bg-white rounded-xl shadow-2xl p-6 sm:w-10/12 mx-10"
-          @click="hideModal()"
+          v-if="showRegModal"
+          class="bg-white rounded-xl shadow-2xl p-6 w-6/12 mx-10"
         >
           <!-- Title -->
           <span class="font-bold block text-2xl mb-3"
-            >🍺 Beer, beer, beer
+            >🍺 Choose a password
           </span>
+          <label class="block">
+            <span class="text-gray-700">Password</span>
+          </label>
+          <input
+            type="password"
+            class="form-input px-4 py-3 rounded-full border border-gray-300"
+            v-model="pass"
+          />
+          <label class="block">
+            <span class="text-gray-700">Confirm Password</span>
+          </label>
+          <input
+            type="password"
+            class="form-input px-4 py-3 rounded-full border border-gray-300"
+            v-model="conpass"
+          />
+          <label v-if="this.passError !== ''" class="inline-flex items-center">
+            <span class="ml-2">{{ this.passError }}</span>
+          </label>
 
           <!-- Buttons -->
           <div class="text-right space-x-5 mt-5">
-            <button @click="hideModal()" class="btn btn-blue">Cancel</button>
+            <button @click="checkPass()" class="btn btn-blue">Submit</button>
+            <button @click="hideModal()" class="btn">Cancel</button>
           </div>
         </div>
       </div>
     </div>
+    <div v-if="this.pageAvailable === 'show'">DATA</div>
   </div>
 </template>
 
 <script>
 export default {
   async asyncData({ params }) {
-    const slug = params.slug
-    return { slug }
+    const slug = params.slug;
+    return { slug };
   },
   data() {
     return {
-      posts: [],
-      available: false,
-      showModal: false,
-    }
+      posts: null,
+      pageAvailable: "unknown",
+      showRegModal: false,
+      pass: "",
+      conpass: "",
+      passError: "",
+    };
   },
   async fetch() {
-    console.log('FETCH CALLED' + `https://jsonkeeper.com/b/${this.slug}`)
-    this.posts = await fetch(`https://jsonkeeper.com/b/${this.slug}`)
+    this.posts = await fetch(
+      `https://no-password-store.netlify.app/.netlify/functions/fauna-crud/${this.slug}`
+    )
       .then((res) => res.json())
-      .catch((e) => console.log(``))
-    console.log('posts', this.posts)
-    if (!this.posts || this.posts?.error == 'not_found') {
-      console.log('Page is available')
-      this.available = true
+      .catch((e) => console.log(``));
+    if (this.posts?.data?.length) {
+      this.pageAvailable = "unavailable";
+    } else {
+      this.pageAvailable = "available";
     }
   },
   methods: {
     visibleModal() {
-      console.log('LDK', this.showModal)
-      this.showModal = !this.showModal
+      this.showRegModal = true;
     },
     hideModal() {
-      console.log('LDK', this.showModal)
-      this.showModal = false
+      this.showRegModal = false;
+    },
+    checkPass() {
+      if (this.pass === this.conpass) {
+        this.passError = "";
+        this.showRegModal = false;
+        this.pageAvailable = "show";
+        this.createSite();
+      } else {
+        this.passError = "Password dont match";
+      }
+    },
+    async createSite() {
+      const ip = await this.$axios.$post(
+        `https://no-password-store.netlify.app/.netlify/functions/fauna-crud/create`,
+        { id: this.slug, data: [{ text: "" }] }
+      );
+      console.log(ip);
     },
   },
-}
+};
 // https://jsonkeeper.com/b/MAM3
 </script>
 
